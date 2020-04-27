8980006c427177e1c3917aa09678097210779b38
Otoniel Vallecillo <orellanaoto@gmail.com>
2020-04-26 19:06:42 -0600

    Nuevo proyecto

---
+++ Conexion/conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+$host= "localhost";
+$usuario= "root";
+$clave= "";
+$bd= "aguasdelvalle";
+
+$conectar=mysqli_connect($host,$usuario,$clave,$bd);
+
+
+//if($conectar){
+//    echo "conectado correctamente";
+//}else{
+//    echo "error al conectar";
+//}
+
+
+?>
